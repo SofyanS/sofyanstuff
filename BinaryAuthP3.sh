417c5e79ab2132f061c38693e524ab0613bf620b
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-21 23:19:37 -0500

    update

--- BinaryAuthP3.sh
+++ BinaryAuthP3.sh
@@ -19,21 +19,7 @@ updateTime: '2020-01-22T02:16:23.564511Z'" > policy.yaml
 
 gcloud beta container binauthz policy import policy.yaml
 
-# Create Nginx pod that will fail to create
-cat << EOF | kubectl create -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: nginx
-spec:
-  containers:
-  - name: nginx
-    image: "gcr.io/${PROJECT_ID}/nginx:latest"
-    ports:
-    - containerPort: 80
-EOF
-
-# Add a path exception too binary auth
+# Add a path exception to binary auth
 echo "admissionWhitelistPatterns:
 - namePattern: gcr.io/google_containers/*
 - namePattern: gcr.io/google-containers/*
@@ -122,6 +108,22 @@ name: projects/$PROJECT_ID/policy
 updateTime: '2020-01-22T02:52:11.826286Z'" > policy.yaml
 
 gcloud beta container binauthz policy import policy.yaml
+
+
+
+# Create Nginx pod that will fail to create
+cat << EOF | kubectl create -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: nginx
+spec:
+  containers:
+  - name: nginx
+    image: "gcr.io/${PROJECT_ID}/nginx:latest"
+    ports:
+    - containerPort: 80
+EOF
 
 # Delete cluster
 cd gke-binary-auth-demo/
