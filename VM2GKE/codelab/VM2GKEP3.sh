9a8f3eef8b43df0ce0280d94c79008ed83b7d97c
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-25 16:04:48 -0500

    Simult delete at start

--- VM2GKE/codelab/VM2GKEP3.sh
+++ VM2GKE/codelab/VM2GKEP3.sh
@@ -1,17 +1,6 @@
-# Create a secret (Not working)
-kubectl create secret generic db-credentials \
---from-file=username=username-mysql-gce.txt \
---from-file=password=password-mysql-gce.txt
+# Verify that the old Application VM has been deleted (Can I just start by running this?)
 
-# Create a configmap
-kubectl create configmap db-config --from-file=db-config=config/config.yaml
+# Create a separate node and then delete it
 
-# Use Sed to modify deployment.yaml so it inserts the project ID
-sed -i "s/\$DEVSHELL_PROJECT_ID/$DEVSHELL_PROJECT_ID/g" deployment.yaml
-
-
-# Verify that the Deployment has been applied to the GKE cluster (Not working)
-#gcloud builds submit --project=$DEVSHELL_PROJECT_ID --config deployment.yaml
-# Check with kubectl get pods and kubectl get deployments
-#kubectl create deployment app-server --image=gcr.io/$DEVSHELL_PROJECT_ID/app:1.0.0
-kubectl create -f deployment.yaml
+gcloud beta container --project "$DEVSHELL_PROJECT_ID" clusters create "mycluster2" --region "us-central1" --node-locations "us-central1-a" --no-enable-basic-auth --cluster-version "1.13.11-gke.23" --machine-type "n1-standard-1" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "1" --enable-cloud-logging --enable-cloud-monitoring --enable-ip-alias --network "projects/$DEVSHELL_PROJECT_ID/global/networks/default" --subnetwork "projects/$DEVSHELL_PROJECT_ID/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
+yes | gcloud container clusters delete mycluster2 --region us-central1
