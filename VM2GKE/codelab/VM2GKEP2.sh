792e680dd05cf9e52340eb86a5b43fbb408dba3b
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-25 13:15:29 -0500

    Fix deployment

--- VM2GKE/codelab/VM2GKEP2.sh
+++ VM2GKE/codelab/VM2GKEP2.sh
@@ -1,6 +1,10 @@
 PROJECT_ID="$(gcloud config get-value project)"
 
 # Push Docker Container to GCR
+#docker build -t app-server:0.1 .
+#docker tag app-server:0.1 gcr.io/$PROJECT_ID/app-server:0.1
+#docker push gcr.io/$PROJECT_ID/app-server:0.1
 docker build -t app-server:0.1 .
-docker tag app-server:0.1 gcr.io/$PROJECT_ID/app-server:0.1
-docker push gcr.io/$PROJECT_ID/app-server:0.1
+docker tag app-server:0.1 gcr.io/$DEVSHELL_PROJECT_ID/app:1.0.0
+docker push gcr.io/$DEVSHELL_PROJECT_ID/app:1.0.0
+
--- VM2GKE/codelab/VM2GKEP3.sh
+++ VM2GKE/codelab/VM2GKEP3.sh
@@ -11,4 +11,6 @@ kubectl create configmap db-config --from-file=db-config=config/config.yaml
 
 # Verify that the Deployment has been applied to the GKE cluster (Not working)
 #gcloud builds submit --project=$PROJECT_ID --config deployment.yaml
-kubectl create deployment app-server --image=gcr.io/$PROJECT_ID/app-server:0.1 # I need to modify the app.py file so it doesn't use config
+# Check with kubectl get pods and kubectl get deployments
+#kubectl create deployment app-server --image=gcr.io/$PROJECT_ID/app-server:0.1
+kubectl create -f deployment.yaml
