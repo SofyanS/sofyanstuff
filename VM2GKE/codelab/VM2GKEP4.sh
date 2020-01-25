5cfd0f42032b382a797a741973b660c4047efb84
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-25 16:53:40 -0500

    revert

--- VM2GKE/codelab/VM2GKEP4.sh
+++ VM2GKE/codelab/VM2GKEP4.sh
@@ -13,8 +13,7 @@ kubectl create configmap db-config --from-file=db-config=config/config.yaml
 sed -i "s/\$DEVSHELL_PROJECT_ID/$DEVSHELL_PROJECT_ID/g" deployment.yaml
 
 
-# Verify that the Deployment has been applied to the GKE cluster (Not working)
-#gcloud builds submit --project=$DEVSHELL_PROJECT_ID --config deployment.yaml
+# Verify that the Deployment has been applied to the GKE cluster
 # Check with kubectl get pods and kubectl get deployments
 #kubectl create deployment app-server --image=gcr.io/$DEVSHELL_PROJECT_ID/app:1.0.0
 kubectl create -f deployment.yaml
